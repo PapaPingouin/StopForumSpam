4781e0b5b766f48b0bb95ec1f547cf193b3c20e7
PapaPinguoin <papapingouin@imw.fr>
2014-05-14 10:23:41 +0200

    french language

---
+++ inc/languages/french/stopforumspam.lang.php
@@ -0,0 +1,14 @@
+<?php
+// ACP
+$l['admin_check_username'] = "Test Username?";
+$l['admin_check_username_desc'] = "Teste le nom d'utilisateur dans la base de StopForumSpam ?";
+$l['admin_check_email'] = "Test Email?";
+$l['admin_check_email_desc'] = "Teste l'email dans la base de StopForumSpam ?";
+$l['admin_check_ip'] = "Test IP Address?";
+$l['admin_check_ip_desc'] = "Teste l'adresse IP dans la base de StopForumSpam ?";
+
+// Front end
+$l['spam_contact_error'] = "StopForumSpam.com n'a pas pu être contacté";
+$l['spam_error'] = "Il y a une erreur lors de votre enregistrement. Merci de contacter l'administrateur ou de réessayer plus tard.";
+$l['spam_blocked'] = "Votre email ou adresse ip correspond à un spammeur identifié, vous ne pouvez pas vous inscrire ici. Si vous pensez que cet une erreur, merci de contacter l'administrateur.";
+?>
